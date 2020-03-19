25d026cce3a01f855231e1f80c0e87ad6656dc0c
namsusz <61674318+namsusz@users.noreply.github.com>
2020-03-19 15:35:45 +0900

    #2.0 end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    //바뀌는 변수
+    isLoading: true,
+    movies: []
+  };
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movies => {
+              return (
+                <Movie
+                  key={movies.id}
+                  id={movies.id}
+                  year={movies.year}
+                  title={movies.title}
+                  summary={movies.summary}
+                  poster={movies.medium_cover_image}
+                  genres={movies.genres}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
 }
 
 export default App;
